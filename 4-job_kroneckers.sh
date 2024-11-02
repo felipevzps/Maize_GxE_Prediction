d1f231281b57f1fe4ec47cf89f12515f21385cbd
felipevzps <felipe.vzps@gmail.com>
2024-11-02 11:51:21 -0300

    adapt for SGE

--- 4-job_kroneckers.sh
+++ 4-job_kroneckers.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=kronecker
-#SBATCH --output=logs/job_kroneckers.txt
-#SBATCH --partition=himem06
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=8
-#SBATCH --time=06:00:00
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
 
 ## configs 
 module purge
 module load gcc/9.3.1 mkl/19.0.5 R/4.2.2
-
 
 ## create Kronecker products between environmental and genomic relationship matrices
 debug=FALSE
