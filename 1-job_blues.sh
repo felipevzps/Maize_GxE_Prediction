f412983ae8989507079bcbf64e12b9881c8225dd
felipevzps <felipe.vzps@gmail.com>
2024-11-02 11:51:11 -0300

    adapt for SGE

--- 1-job_blues.sh
+++ 1-job_blues.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=blues
-#SBATCH --output=logs/job_blues.txt
-#SBATCH --partition=comp01
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=8
-#SBATCH --time=01:00:00
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
 
 ## configs 
 module purge
 module load gcc/9.3.1 mkl/19.0.5 intel/19.0.5 R/4.2.2
-
 
 ## run
 Rscript src/blues.R > logs/blues.txt
